c7dc72fd383101fc12c2d76074de24f71d19ed26
Adam Cummins A <adam.a.cummins@ericsson.com>
2024-08-02 14:08:57 +0100

    added extra nodes to seeding

--- spring-project/src/main/java/com/example/springproject/SeedDB.java
+++ spring-project/src/main/java/com/example/springproject/SeedDB.java
@@ -20,5 +20,18 @@ public class SeedDB {
                 "node 3", "cyprus", 35.1264, 33.4299);
         jdbcTemplate.update("insert into NODE(name, location, latitude, longitude) values(?, ?, ?, ?)",
                 "node 4", "beijing", 39.9042, 116.4074);
+        jdbcTemplate.update("insert into NODE(name, location, latitude, longitude) values(?, ?, ?, ?)",
+                "node 5", "tralee", 52.2713, -9.6999);
+        jdbcTemplate.update("insert into NODE(name, location, latitude, longitude) values(?, ?, ?, ?)",
+                "node 6", "bundoran", 54.4791, -8.2779);
+        jdbcTemplate.update("insert into NODE(name, location, latitude, longitude) values(?, ?, ?, ?)",
+                "node 7", "westport", 53.8011, -9.5222);
+        jdbcTemplate.update("insert into NODE(name, location, latitude, longitude) values(?, ?, ?, ?)",
+                "node 8", "gorey", 52.6753, -6.2959);
+        jdbcTemplate.update("insert into NODE(name, location, latitude, longitude) values(?, ?, ?, ?)",
+                "node 9", "limerick", 52.6638, -8.6267);
+        jdbcTemplate.update("insert into NODE(name, location, latitude, longitude) values(?, ?, ?, ?)",
+                "node 10", "cork", 51.8985, -8.4756);
+
     }
 }
